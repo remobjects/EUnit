db5160761ac09db2ebb95b61ff3454333210eabc
marc hoffman <mh@remobjects.com>
2016-12-15 10:31:17 -0400

    Added Island EUnit projects (not implemented/working yet)

---
+++ Sources/Discovery/Island/IslandDiscovery.pas
@@ -0,0 +1,24 @@
+﻿namespace RemObjects.Elements.EUnit;
+
+interface
+
+type
+  Discovery = public partial static class
+  public
+    method FromModule: ITest;
+    method FromModuleAsync(OnCompleted: Action<IAsyncResult<ITest>>; Token: ICancelationToken := nil);
+  end;
+
+implementation
+
+class method Discovery.FromModule: ITest;
+begin
+  exit new ModuleDiscovery().Discover;
+end;
+
+class method Discovery.FromModuleAsync(OnCompleted: Action<IAsyncResult<ITest>>; Token: ICancelationToken := nil);
+begin
+  new ModuleDiscovery().DiscoverAsync(OnCompleted, Token);
+end;
+
+end.
---
+++ Sources/Discovery/Island/ModuleDiscovery.pas
@@ -0,0 +1,51 @@
+﻿namespace RemObjects.Elements.EUnit;
+
+interface
+
+uses
+  RemObjects.Elements.EUnit.Reflections;
+
+type
+  ModuleDiscovery = assembly class (BaseDiscovery)
+  public
+    constructor;
+    method Filter: List<ITest>; override;
+  end;
+
+implementation
+
+method ModuleDiscovery.Filter: List<ITest>;
+begin
+  result := new List<ITest>;
+
+  var Count := objc_getClassList(nil, 0);
+  if Count <= 0 then
+    exit;
+
+  var Classes := new unretained &Class[Count];
+  Count := objc_getClassList(Classes, Count);
+
+  for i: Integer := 0 to Count - 1 do begin
+    var lClass: unretained &Class := Classes[i];
+    var Super: &Class := class_getSuperclass(lClass);
+
+    while Super <> nil do begin
+
+      //inherits from Testcase
+      if Super = Test.class then begin
+        var Instance := Foundation.NSClassFromString(class_getName(lClass)).alloc.init;
+        var Abc := new InstanceDiscovery([Instance]);
+        result.Add(Abc.Filter);
+        break;
+      end;
+
+      Super := class_getSuperclass(Super);
+    end;
+  end;
+end;
+
+constructor ModuleDiscovery;
+begin
+end;
+
+end.
