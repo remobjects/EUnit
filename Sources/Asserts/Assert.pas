020d3e8bb5d7a768e3681bed49f8e29d17a4e504
marc hoffman <mh@remobjects.com>
2026-02-08 11:52:11 -0400

    Dont emit success for each Assert

--- Sources/Asserts/Assert.pas
+++ Sources/Asserts/Assert.pas
@@ -65,9 +65,9 @@ type
     method Success(aIgnoredMessage: String; aFile: String := currentFileName(); aLine: Integer := currentLineNumber(); aClass: String := currentClassName(); aMethod: String := currentMethodName()); override;
     begin
       {$IF NOT WINDOWS_PHONE AND NOT NETFX_CORE}
-      if ConsoleTestListener.EmitParseableSuccessMessages then begin
-        writeLn(String.Format("TEST-SUCCEEDED,{0},{1},{2}.{3},{4}", aFile, if aLine > 0 then aLine else "", aClass:Trim, aMethod:Trim, "Succeeded"));
-      end;
+      //if ConsoleTestListener.EmitParseableSuccessMessages then begin
+        //writeLn(String.Format("TEST-SUCCEEDED,{0},{1},{2}.{3},{4}", aFile, if aLine > 0 then aLine else "", aClass:Trim, aMethod:Trim, "Succeeded"));
+      //end;
       {$ENDIF}
     end;
 
